a37f37100d0af1e47138599634adb651715935ec
baghiz zuhdi adzin <baghizzhd@gmail.com>
2021-01-18 14:40:03 +0700

    Add project files.

---
+++ WindowsFormsApp00/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp00
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox21.Hide();
+            pictureBox22.Hide();
+        }
+        public int bagis = 2;
+
+
+        private void B_Click(object sender, EventArgs e)
+        {if (bagis % 2 == 1)
+            {
+                B.Hide();
+                pictureBox21.Show();
+            }
+        else if (bagis % 2 == 0)
+            {
+                B.Hide();
+                pictureBox22.Show();
+            }
+        }
+    }
+}
